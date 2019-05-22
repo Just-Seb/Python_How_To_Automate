3128d59cd6ed7b1b6fbe0a94abe8c7056cdff188
Sebastian Scurtescu <sebastian.scurtescu@gmail.com>
2019-05-22 11:02:40 -0700

    I forgot one

--- quickWeather.py
+++ quickWeather.py
@@ -10,7 +10,7 @@ if len(sys.argv) < 2:
 location = ' '.join(sys.argv[1:])
 
 # Download the jason data from OpenWeatherMap.org's API.
-url = 'hhtp://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3' % (location)
+url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3' % (location)
 response = requests.get(url)
 response.raise_for_status()
 
@@ -25,4 +25,4 @@ print('Tomorrow: ')
 print(w[1]['weather'][1]['main'], '-', w[1]['weather'][0]['description'])
 print()
 print('Day after tomorrow: ')
-print(w[0]['weather'][2]['main'], '-', w[2]['weather'][0]['description'])
+print(w[2]['weather'][2]['main'], '-', w[2]['weather'][0]['description'])
